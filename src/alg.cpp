01350d9573704ef6d2ed6ace9ced49f42845b42e
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-02-23 15:29:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -54,10 +54,10 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  const std::int64_t size = 10000;
+  const std::int64_t size = 5000;
   std::int64_t arr[size] = { 0 };
   std::int64_t j = 0, sum = 0;
-  for (int i = 2; i < 10000; i++) {
+  for (int i = 2; i < 5000; i++) {
     if (checkPrime(i)) {
       arr[j] = i;
       j++;
