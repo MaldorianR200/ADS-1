2dd007536b2a54a81df27f406c926496dc3163c7
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-02-23 12:30:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,67 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  std::int64_t count_d = 0;
+  for (std::int64_t i = 2; i <= value / 2; i++) {
+    if (value % i == 0) {
+      ++count_d;
+    }
+  }
+  if (count_d == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  const std::int64_t size = 10000;
+  std::int64_t arr[size] = { 0 };
+  std::int64_t j = 0;
+  for (int i = 2; i < 10000; i++) {
+    if (checkPrime(i)) {
+      arr[j] = i;
+      j++;
+    }
+  }
+  for (int i = 0; i < size; i++) {
+    if (n - 1 == i) {
+      return arr[i];
+    }
+  }
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  const std::int64_t size = 10000;
+  std::int64_t arr[size] = { 0 };
+  std::int64_t j = 0;
+  for (int i = 2; i < 10000; i++) {
+    if (checkPrime(i)) {
+      arr[j] = i;
+      j++;
+    }
+  }
+  for (int i = 0; i < 10000; i++) {
+    if (arr[i] == value) {
+      return arr[i + 1];
+    }
+  }
+  return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  const std::int64_t size = 10000;
+  std::int64_t arr[size] = { 0 };
+  std::int64_t j = 0;
+  for (int i = 2; i < 10000; i++) {
+    if (checkPrime(i)) {
+      arr[j] = i;
+      j++;
+    }
+  }
+  for (int i = 0; i < hbound; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
